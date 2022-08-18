0c2dae29045b11ba7f4fbafde083b603a3419b92
Abdellatif Amireh <abdellatif.amireh95@gmail.com>
2022-08-18 16:09:37 +0400

    add private string

---
+++ lib/constants/strings_private.dart
@@ -0,0 +1,3 @@
+// This file will NOT push in github for security reason
+const client_id = "Mjg1MDA1NzB8MTY2MDcxNjExOC44ODM1MTM";
+const client_secret = "bcb63ebb39f6876975be9ed674ff6c0c6e3d0363cc0b075c65f3112db72a608e";
